f8fc1744671f609cd5eca58a88c2bb1a1806e183
Dalal Alsabah <dasabah@gmail.com>
2022-11-13 19:37:44 +0300

    added a get function

--- main.dart
+++ main.dart
@@ -9,9 +9,33 @@ void main() {
   print(movie['ratings']);
   Movie movie2 = Movie(name: 'Dalal', genre: 'thriller', rating: [1, 2, 3]);
   movie2.printDetails();
+  Movie movie3 = Movie(name: 'batman', genre: 'comedy', rating: [3]);
+  movie3.printDetails();
 }
 
 class Movie {
+  final String name;
+  final String genre;
+  final List<double> rating;
+
+  double get averageRating {
+    double sum = 0.0;
+    for (var r in rating) {
+      sum += r;
+    }
+    return sum / rating.length;
+  }
+
+  Movie({required this.name, required this.genre, required this.rating});
+
+  void printDetails() {
+    print('$name');
+    print('$genre');
+    print('$rating');
+  }
+}
+
+/* class Movie {
   String name;
   String genre;
   List<double> rating;
@@ -22,4 +46,4 @@ class Movie {
   void printDetails() {
     print('name:$name genre:$genre rating:$rating');
   }
-}
+}*/
